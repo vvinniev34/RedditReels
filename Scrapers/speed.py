90b46fe677a261fd7223f1809883e12d65b67af0
Yooshkimchi <joshuakim20012345@gmail.com>
2023-09-02 21:59:17 -0400

    Sped up audio playback for the mp3

---
+++ Scrapers/speed.py
@@ -0,0 +1,11 @@
+from pydub import AudioSegment
+from datetime import date
+# or
+today = date.today().strftime("%Y-%m-%d")
+path = f"redditPosts/{today}/Texts" + "/AITAfortellingmywifethelockonm.mp3"
+    
+# export to mp3
+sound = AudioSegment.from_file(path)
+velocidad_X = 1.25 
+so = sound.speedup(velocidad_X, 150, 25)
+so.export(path[:-4] + '_Out.mp3', format = 'mp3')
