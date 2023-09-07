f6cf1159c41855e7effa7fd99ae81159758e2822
Vincent Liu <vvinniev34@gmail.com>
2023-09-07 16:49:35 -0500

    update run.py

--- video_creation/run.py
+++ video_creation/run.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 if __name__ == "main":
@@ -6,3 +7,11 @@ if __name__ == "main":
     subprocess.run(["python", "scrapeLinks.py"])
     subprocess.run(["python", "textToSpeech.py"])
     subprocess.run(["python", "videoMaker.py"])
+
+    today = "2023-09-02"
+    folder_path = f"RedditPosts/{today}/Texts"
+    for subreddit in os.listdir(folder_path):
+        post_path = f"{folder_path}/{subreddit}"
+        for post in os.listdir(post_path):
+            if post.endswith("F.mp3"):
+                subprocess.run(["python", "uploadVideo", "--file", f"{post_path}/{post}"])
