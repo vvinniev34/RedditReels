76a3818ed700c672862fec0ef053d89f60fa1f3c
Vincent Liu <vvinniev34@gmail.com>
2023-09-07 12:19:13 -0500

    oauth service account

---
+++ video_creation/oauth.py
@@ -0,0 +1,22 @@
+import os
+from google.oauth2 import service_account
+from google.auth.transport.requests import Request
+
+# Path to the service account key JSON file
+key_path = 'path/to/your-service-account-key.json'
+
+# Define the scopes (permissions) you need
+scopes = ['https://www.googleapis.com/auth/calendar.readonly']
+
+# Create credentials from the service account key file
+credentials = service_account.Credentials.from_service_account_file(
+    key_path, scopes=scopes
+)
+
+# If the token has expired, refresh it
+if credentials.expired:
+    credentials.refresh(Request())
+
+# Access token for making API requests
+access_token = credentials.token
+print(f'Access Token: {access_token}')
