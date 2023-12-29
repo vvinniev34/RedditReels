36339b6d87639d452da4054e866b6dcc1ef7c5fa
Vincent Liu <vvinniev34@gmail.com>
2023-12-29 17:42:05 -0600

    run.py comments

--- video_creation/run.py
+++ video_creation/run.py
@@ -3,11 +3,16 @@ import subprocess
 
 if __name__ == "main":
     # Run the Python files
+    # get top weekly posts
     subprocess.run(["python", "topKWeeklyPostsScraper.py"])
+    # get post content
     subprocess.run(["python", "scrapeLinks.py"])
+    # convert post content to mp3
     subprocess.run(["python", "textToSpeech.py"])
+    # create videos
     subprocess.run(["python", "videoMaker.py"])
 
+    # upload videos
     today = "2023-09-02"
     folder_path = f"RedditPosts/{today}/Texts"
     for subreddit in os.listdir(folder_path):
